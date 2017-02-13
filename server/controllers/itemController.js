732631232f09957e70186068e48c97808cc61f86
Your Name <saurajit@ZALONILABS.COM>
2017-02-14 02:26:49 +0530

    Improved items router and added item Controller

---
+++ server/controllers/itemController.js
@@ -0,0 +1,16 @@
+var Controller = {
+  createItem: function (item) {
+    console.log('Item created');
+  },
+  retrieveItem: function (itemId) {
+
+  },
+  updateItem: function (itemId, itemObject) {
+
+  },
+  deleteItem: function (itemId) {
+
+  }
+};
+
+module.exports = Controller;
--- server/routes/items.js
+++ server/routes/items.js
@@ -1,25 +1,27 @@
-
-
 var routes = function (routes) {
+  var itemController = require('../controllers/itemController');
+
   // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
   routes.get('/', function(req, res) {
       res.json({ message: 'hooray! welcome to our api!' });   
   });
 
-  routes.get('/items/:itemId', function(req, res) {
-    console.log(req);
-    res.json({ result: req.params, uri: req.route.path});
-  });
+  routes.route('/items/:itemId')
 
-  routes.delete('/items/:itemId', function(req, res) {
+  .get(function(req, res) {
+    itemController.saveItem("Test");
+    res.json({ result: req.params, uri: req.route.path});
+  })
+
+  .delete(function(req, res) {
     var response = {
       params: req.params
     };
     console.log("Request for deleting item with id: " + req.params.itemId);
     res.json({ result: response, uri: req.route.path});
-  });
+  })
 
-  routes.put('/items/:itemId', function(req, res) {
+  .put(function(req, res) {
     var response = {
       params: req.params,
       body: req.body
